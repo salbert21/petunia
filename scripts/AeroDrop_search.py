0f3cb8ebcc058a695472dd5db54268453788b787
Samuel <samuel.albert@colorado.edu>
2020-07-03 16:34:40 -0500

    compute A from m and CD

--- scripts/AeroDrop_search.py
+++ scripts/AeroDrop_search.py
@@ -46,8 +46,14 @@ class Outs:
 # =============================================================================
 
 def main(params, tspan, events, outs):
-    # get CD from BC
-    params.CD = params.m / (params.BC * params.A)
+    # get A from CD and BC
+    params.A = params.m / (params.BC * params.CD)
+    
+    # get Rn from A, assuming Rn/Rb = 1/2
+    params.Rn = np.sqrt(params.A / np.pi) / 2
+    
+    # get CL from L/D and CD
+    params.CL = params.CD * params.LD
     
     ### CONVERT TO INERTIAL INITIAL STATE
     params.x0, params.v0 = LLAEHV2RV(params.lat, params.lon, params.alt,
@@ -220,10 +226,10 @@ params.atmdat = np.array([atmdata['Hgtkm'], atmdata['DensMean']])
 # params.atmdat = rhoTable
 
 ### VEHICLE PARAMS (NOT CHANGED DURING GRID SEARCH)
-params.m = 2000 # kg 
-params.Rn = 0.25 # m, effective nose radius # TODO: CHANGE THIS VALUE
-params.A = 30
-params.CL = 0
+params.m = 2920 # kg, roughly MSL mass
+params.CD = params.m / (115 * np.pi * (4.5/2)**2) # roughly MSL CD
+
+params.LD = 0
 
 ### INITIAL STATE (COMPONENTS NOT CHANGED DURING GRID SEARCH)
 params.lat = 40
@@ -252,7 +258,7 @@ event2.terminal = True
 events = (event1, event2)
 
 ### GRID SEARCH
-efpaList = np.arange(-3.5, -7, -0.05)
+efpaList = np.arange(-3, -7.5, -0.02)
 BCList = np.arange(10, 200, 5)
 outsList = []
 
