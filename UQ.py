709939ffac4a0060a872fee7153be3b36b901ff4
Samuel <samuel.albert@colorado.edu>
2020-10-20 14:26:18 -0600

    added module for KLE and PCE functions

---
+++ UQ.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+UQ.py:
+    contains functions related to KLE, PCE, and other UQ methods for petunia
+    
+Created on Tue Oct 20 14:25:03 2020
+
+@author: Samuel Albert
+"""
+
+
+import numpy as np
